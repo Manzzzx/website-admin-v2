69206ced647cca68d1fb696c1ff0e10f719a1443
Firmansyah <147714843+Manzzzx@users.noreply.github.com>
2024-11-25 18:08:37 +0700

    hapus toko (admin)

--- app/(dashboard)/[storeId]/(routes)/settings/components/settings-form.tsx
+++ app/(dashboard)/[storeId]/(routes)/settings/components/settings-form.tsx
@@ -53,12 +53,27 @@ export const SettingsForm: React.FC<SettingsFormProps> = ({
         }
     }
 
+    const onDelete = async () => {
+        try {
+            setLoading(true)
+            await axios.delete(`/api/stores/${params.storeId}`)
+            router.refresh()
+            router.push("/")
+            toast.success("Toko berhasil dihapus")
+        } catch (error) {
+            toast.error("Cek kembali data dan koneksi mu")
+        } finally {
+            setLoading(false)
+            setOpen(false)
+        }
+    }
+
     return (
         <>
         <AlertModal
             isOpen={open}
             onClose={() => setOpen(false)}
-            onConfirm={() => {}}
+            onConfirm={onDelete}
             loading={loading}
         />
             <div className="flex items-center justify-between">
